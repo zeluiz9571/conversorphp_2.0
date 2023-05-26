b46f98212513bf72f53ec032a8a88643dff9d3a8
zeluiz9571 <130668873+zeluiz9571@users.noreply.github.com>
2023-05-26 15:44:24 -0300

    Commit do zé

---
+++ conversor.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Conversor de Temperatura</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <?php
+
+        echo "<br> <br> <br> <center>";
+
+        $op = $_POST["tipoTemp"];
+
+        if($op == "1"){
+            $cel = $_POST["temp"];
+            $fah = round(9 * $cel / 5 + 32);
+            echo "<h1> ". $cel. "ºC = " .$fah. "°F </h1> <br>";
+        }else{
+            echo "<h2> Converter Fahrenheit em Celsius! </h2>";
+        }
+
+        if($op == "2"){
+            $fah = $_POST["temp"];
+            $cel = round(($fah - 32) / 9 * 5);
+            echo "<h1> ". $fah. "°F = " .$cel. "ºC </h1> <br>";
+        }else{
+            echo "<h2> Converter Celisius em Fahrenheit! </h2>";
+        }
+
+        echo "<br> <br> <br> </center>"
+
+    ?>
+
+        <a href="index.php"> <center> Voltar </center></a>
+
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Conversor de Temperatura</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <script src='main.js'></script>
+</head>
+<body>
+    <h1><center>Conversor de Temperatura</center></h1>
+    <form action="conversor.php" method="post">
+        <lable for="tipoConversao"> Tipo de conversão: 
+            <select id="op" name="tipoTemp">
+                <option value="1">ºC para ºF</option>
+                <option value="2">ºF para ºC</option>
+            </select>
+        </lable><br><br>
+        <label for="temperatura">Temperatura:</label>
+        <input type="number" name="temp">
+        <input type="submit">
+
+    </form>
+</body>
+</html>
